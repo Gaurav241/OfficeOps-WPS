2fb2f619310cd0e3d7a26c4cc2411f104ee6aa63
Gaurav241 <gaurav.govinda2420@gmail.com>
2023-11-22 17:40:28 +0530

    OfficeOps: No need to have local database

--- office_ops/settings.py
+++ office_ops/settings.py
@@ -88,13 +88,6 @@ WSGI_APPLICATION = 'office_ops.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-DATABASES = {
     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
