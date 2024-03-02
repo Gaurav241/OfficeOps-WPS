cef6d2a1a0f1c78eebcfa346469dafb421570978
Gaurav241 <gaurav.govinda2420@gmail.com>
2024-03-02 20:54:57 +0530

    OfficeOPS: Make debug as true

--- office_ops/settings.py
+++ office_ops/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.environ.get("SECRET_KEY", default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", default="False").lower() == "true"
+DEBUG = True
 
 # ALLOWED_HOSTS
 ALLOWED_HOSTS = ['*']
